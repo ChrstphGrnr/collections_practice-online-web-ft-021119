b2c472f2743fe3b78520b4c65196669b5aabf5ec
Christoph Gruener <christophgruener@me.com>
2019-02-11 21:12:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,6 +22,7 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  array.insert
+  array.insert(destination_index, array[index])
+  array[index].delete
 end
 
