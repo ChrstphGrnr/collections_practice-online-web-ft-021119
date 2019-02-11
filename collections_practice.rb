a266d1016daebbb8c94e220ce911bcdf326adb6f
Christoph Gruener <christophgruener@me.com>
2019-02-11 21:19:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,6 +22,6 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  array[destination_index] = array.slice![index]
+  array[destination_index] = array.slice!(index)
 end
 
